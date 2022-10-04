6e60ca5ab27cd6e60247d8c42fb42a2418147f96
Gerard Moreno <gerardmoreno@iesebre.com>
2022-10-04 14:24:06 +0200

    modificat

--- UF1/pt2.py
+++ UF1/pt2.py
@@ -4,6 +4,11 @@ Realitza els següents programes i lliura el codi font:
 
 Crea un programa que a partir d'una cadena de 4 caràcters (numèrics) preguntada a l'usuari, (com "123456") imprimeixi la suma dels números que la formen. 
 Teniu en compte que només s'ha d'utilitzar un input, estem treballant les cadenes.
+'''
+#numinput = input("Escriu 4 numeros: ")
+#print(int(numinput[0])+int(numinput[2])+int(numinput[4])+int(numinput[6]))
+
+'''
 Consulta els mètodes "built-in" que teniu disponibles per a cadenes i crea un programa que a partir d'una frase donada per l'usuari calculi:
 Número de caràcters.
 Número de paraules.
@@ -11,6 +16,19 @@ Frase  amb totes les paraules en majúscula.
 Frase  amb totes les paraules en minúscula.
 Preguntat un caràcter a l'usuari retorni la posició de la primera coincidència trobada a la frase.
 Preguntat un caràcter a l'usuari retorni la posició de la darrera coincidència trobada a la frase.
- Preguntat un caràcter a l'usuari retorni el número de coincidències trobades a la frase.
+Preguntat un caràcter a l'usuari retorni el número de coincidències trobades a la frase.
 Mostri el número de vocals del text (has d'optimitzar al màxim aquest codi!!).
-Modifica el primer programa per a que abans de donar el resultat mostri si és cert que la cadena només conté números.'''
+Modifica el primer programa per a que abans de donar el resultat mostri si és cert que la cadena només conté números.
+'''
+
+#userinput = input('Escriu una frase: ')
+userinput = str('Hola que tal. Tinc 54 anys')
+caracters = len(userinput)
+paraules = len(userinput.split())
+majuscula = (userinput.upper())
+miniscula =(userinput.lower())
+pos_inicial = ()
+pos_final = ()
+coincidencia = ()
+vocals_total = ()
+nomes_num = ()
