e25c5ae94881562720c552c3e735bb42fa6268af
Gerard Moreno <gerardmoreno@iesebre.com>
2022-10-04 13:07:48 +0200

    asd

---
+++ UF1/pt2.py
@@ -0,0 +1,16 @@
+
+'''
+Realitza els següents programes i lliura el codi font:
+
+Crea un programa que a partir d'una cadena de 4 caràcters (numèrics) preguntada a l'usuari, (com "123456") imprimeixi la suma dels números que la formen. 
+Teniu en compte que només s'ha d'utilitzar un input, estem treballant les cadenes.
+Consulta els mètodes "built-in" que teniu disponibles per a cadenes i crea un programa que a partir d'una frase donada per l'usuari calculi:
+Número de caràcters.
+Número de paraules.
+Frase  amb totes les paraules en majúscula.
+Frase  amb totes les paraules en minúscula.
+Preguntat un caràcter a l'usuari retorni la posició de la primera coincidència trobada a la frase.
+Preguntat un caràcter a l'usuari retorni la posició de la darrera coincidència trobada a la frase.
+ Preguntat un caràcter a l'usuari retorni el número de coincidències trobades a la frase.
+Mostri el número de vocals del text (has d'optimitzar al màxim aquest codi!!).
+Modifica el primer programa per a que abans de donar el resultat mostri si és cert que la cadena només conté números.'''
