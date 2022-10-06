75564771fc9cddaacf6689a5d57d5e37209f37bb
Gerard Moreno <gerardmoreno@iesebre.com>
2022-10-06 13:59:17 +0200

    hollaa

--- UF1/pt2.py
+++ UF1/pt2.py
@@ -5,8 +5,9 @@ Realitza els següents programes i lliura el codi font:
 Crea un programa que a partir d'una cadena de 4 caràcters (numèrics) preguntada a l'usuari, (com "123456") imprimeixi la suma dels números que la formen. 
 Teniu en compte que només s'ha d'utilitzar un input, estem treballant les cadenes.
 '''
-#numinput = input("Escriu 4 numeros: ")
-#print(int(numinput[0])+int(numinput[2])+int(numinput[4])+int(numinput[6]))
+
+numinput = input("Escriu 4 numeros separats amb un espai: ")
+print(int(numinput[0])+int(numinput[2])+int(numinput[4])+int(numinput[6]))
 
 '''
 Consulta els mètodes "built-in" que teniu disponibles per a cadenes i crea un programa que a partir d'una frase donada per l'usuari calculi:
@@ -21,22 +22,43 @@ Mostri el número de vocals del text (has d'optimitzar al màxim aquest codi!!).
 Modifica el primer programa per a que abans de donar el resultat mostri si és cert que la cadena només conté números.
 '''
 
-#userinput = input('Escriu una frase: ')
-userinput = str('Hola que tal. Tinc 54 anys')
+#Sense preguntar a l'usuari --> userinput = str('Hola que tal. Tinc 54 Anys')
+#Pregunta la frase al usuari
+userinput = input('Escriu una frase: ')
+
+#Calcul numero de caracters
 caracters = len(userinput)
 print("Caracters: ",caracters)
+
+#Calcul numero de paraules
 paraules = len(userinput.split())
 print("Paraules: ",paraules)
+
+#Posar la frase en majuscula
 majuscula = (userinput.upper())
 print("Tot en majuscules: ",majuscula)
+
+#Posar  la frase en miniscula
 miniscula =(userinput.lower())
 print("Tot en minuscules: ",miniscula)
-pos_inicial = input()
-print(pos_inicial)
-pos_final = (userinput)
-print(pos_inicial)
-coincidencia = (userinput)
-print(coincidencia)
-vocals_total = (userinput.count('a')+userinput.count('e')+userinput.count('i')+userinput.count('o')+userinput.count('u'))
-print(vocals_total)
-vocals_inicial = (userinput)
+
+#Buscar el primer caracter de la frase
+caracter_inicial = input("Digues un caracter: ")
+print("El primer caracter es troba en la posicio: ",userinput.find(caracter_inicial))
+
+#Buscar el ultim caracter de la frase
+caracter_final = input("Digues un caracter: ")
+print("El ultim caracter es troba en la posicio: ",userinput.rfind(caracter_final))
+
+#Calcular el total de caracters de la frase
+coincidencia = input("Digues un caracter: ")
+print("El total de coincidencies es: ",userinput.count(coincidencia))
+
+#Posar la frase en miniscula i contar el total de vocals
+vocal_minuscula = (userinput.lower())
+#vocals_total = (vocal_minuscula.count('a')+vocal_minuscula.count('e')+vocal_minuscula.count('i')+vocal_minuscula.count('o')+vocal_minuscula.count('u'))
+print("Les vocals totals son: ",vocal_minuscula.count('a')+vocal_minuscula.count('e')+vocal_minuscula.count('i')+vocal_minuscula.count('o')+vocal_minuscula.count('u'))
+
+#Mostrar si el input introduit es numeric
+numinput = input("Escriu 4 numeros seguits: ")
+print(numinput.isnumeric())
