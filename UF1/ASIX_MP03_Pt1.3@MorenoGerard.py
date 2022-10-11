5d4a5fae85d83912ace9e8e2783ce947127d7475
Gerard Moreno <gerardmoreno@iesebre.com>
2022-10-11 13:09:21 +0200

    ggjnsRVBER HOLA

---
+++ UF1/ASIX_MP03_Pt1.3@MorenoGerard.py
@@ -0,0 +1,10 @@
+'''
+Crea un programa que demani una cadena de 4 caràcters (numèrics) a l'usuari, (com "1234") els emmagatzemi a una llista convertits en enters i imprimeixi la suma dels números que la formen. Heu d'utilitzar una funció interna per a fer la suma.
+https://www.w3schools.com/python/python_ref_functions.asp
+Demana a l'usuari un número i afegeix-lo al final de la llista amb un mètode de llista.
+Ara elimina aquest número de la llista amb un mètode de llista.
+Ordena la llista amb un mètode de llista.
+Mostra el seu número màxim i el seu mínim, extrets amb funcions internes.
+Calcula la mitjana aritmètica de la llista a partir de les funcions internes sum() i len().
+'''
+
---
+++ UF1/llistes.py
@@ -0,0 +1,5 @@
+pesos=[("pepe", "cisco", "joel"),[45,78,64],[55,75,67],[60,69,63]]
+print(pesos[0][0],pesos[1][0],pesos[2][0],pesos[3][0])
+
+pesos2=[["pepe",45,78,64],["cisco",55,75,67],["joel",60,69,63]]
+print(pesos2[0][0],pesos2[0][1:])
