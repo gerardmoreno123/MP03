ed0ef943df5b654db72fd76a725133fd472c66e2
Gerard Moreno <gerardmoreno@iesebre.com>
2022-10-13 11:33:07 +0200

    hksdfghjndf

--- UF1/ASIX_MP03_Pt1.4(Extra)@MorenoGerard.py
+++ UF1/ASIX_MP03_Pt1.4(Extra)@MorenoGerard.py
@@ -1,4 +1,5 @@
 #DNI de prova 74870769L
-dni= (input("Introduzca el número del DNI:"))
-lletres='TRWAGMYFPDXBNJZSQVHLCKE'
-print('La letra del DNI es: ', lletres[dni%23])
+dni= int(input("Introdueix els numeros del dni(sense lletra):"))
+lletres={0:"T",1:"R",2:"W",3:"A",4:"G",5:"M",6:"Y",7:"F",8:"P",9:"D",10:"X",11:"B",12:"N",13:"J",14:"Z",15:"S",16:"Q",17:"V",18:"H",19:"L",20:"C",21:"K",22:"E"}
+calcul = dni%23
+print('La letra del DNI es: ', lletres[calcul])
