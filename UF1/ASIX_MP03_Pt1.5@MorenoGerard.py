b37341dec048b337ecdee68adade8d4b2723e6a2
Gerard Moreno <gerardmoreno@iesebre.com>
2022-10-25 12:38:06 +0200

    hola

--- UF1/ASIX_MP03_Pt1.5@MorenoGerard.py
+++ UF1/ASIX_MP03_Pt1.5@MorenoGerard.py
@@ -15,8 +15,8 @@ Introduïu puntuació: 7.5 -> Bé
 Introduïu puntuació: 0.5 -> Insuficient
 '''
 
-nota = float(input("Digues una puntuació entre 0 i 10: "))
 try:
+    nota = float(input("Digues una puntuació entre 0 i 10: "))
     if (nota >10 or nota < 0):
         print("Fora del rang")
     elif (nota >= 9):
@@ -31,33 +31,30 @@ try:
         print("Insuficient")
     else:
         print("Puntiació incorrecta")
+
 except ValueError:
-    print("Puntiació incorrecta")
+    print("Puntiació Incorrecta")
 
-
-'''
-nota = input("Digues una puntuació entre 0 i 10: ")
-
-if (float(nota) >10 or float(nota) < 0):
-    print("Fora del rang")
-elif (float(nota) >= 9):
-    print("Exel·lent")
-elif (float(nota) >= 8):
-    print("Notable")
-elif (float(nota) >= 7):
-    print("Bé")
-elif (float(nota) >= 5):
-    print("Suficient")
-elif (float(nota) < 5):
-    print("Insuficient")
-else:
-    print("Puntiació incorrecta")
-'''
 '''
 Tasca 2:
 Escriu un programa que demani l'any actual i un altre any qualsevol. El resultat ha de mostrar quants anys han passat des de l'any introduït o quants anys falten.
 Ara milloreu el programa per a fer que quan la diferència sigui només d'un any, ens digui que només falta un any.
 '''
+
+
+try:
+    actual = int(input("Quin any es: "))
+    any_qualsevol = int(input("Digues un any qualsevol: "))
+    resta = actual - any_qualsevol
+    if (resta>1):
+        print("Han pasat", resta)
+    elif (resta>=1):
+        print("falta",resta,"any")
+    elif (resta<1):
+        print("Falten", abs(resta))
+except ValueError:
+    print("Error")
+
 
 '''
 Tasca 3:
@@ -67,3 +64,25 @@ Exemple d'ús:
 import random
 numero = random.randint(1, 6)
 '''
+import random
+
+try:
+    dau1 = input("Jugador 1 tira el dau\n ")
+    dau2 = input("Tugador 2 tira el dau\n ")
+
+    if (dau1 == "tirar" or dau2 == "tirar"):
+        dau1 = random.randint(1, 6)
+        dau2 = random.randint(1, 6)
+        print("El jugador 1 ha tet ",dau1)
+        print("El jugador 2 ha tet ",dau2)
+        if (dau1>dau2):
+            print("Guanya el jugador 1")
+        elif (dau1<dau2):
+            print("Guanya el jugador 2")
+        elif (dau1==dau2):
+            print("Empat")      
+    else:
+        print("Un dels jugadors no sap tirar el dau")   
+
+except ValueError:
+    print("Error")
